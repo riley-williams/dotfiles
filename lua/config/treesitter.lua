25387b96a975432a4850e598be2ef1d87112c700
Riley Williams <williams.riley@icloud.com>
2023-01-25 19:35:44 -0500

    Adds additional treesitter languages

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,7 +1,7 @@
 local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
-	ensure_installed = { "swift", "rust", "lua", "json", "toml" },
+	ensure_installed = { "swift", "rust", "lua", "json", "toml", "python", "bash", "graphql", "gitignore", "markdown", "markdown_inline", "pioasm", "sql", "yaml" },
 	sync_install = false,
     highlight = {
         enable = true
