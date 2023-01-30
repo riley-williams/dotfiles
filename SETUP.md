304b8e256cb50dd025bbf49ec06489aa80a568d9
Riley Williams <williams.riley@icloud.com>
2023-01-30 17:43:52 -0500

    tweaks tab complete

--- SETUP.md
+++ SETUP.md
@@ -1,8 +1,13 @@
 # Setup
 
+requires nvim nightly (0.9+)
+
 ## Required dependencies
 
-node (nvm), swift, rust
+swift, rust
+macOS:
+- `brew install ripgrep`
+- `brew install nvm`
 
 Linux:
 - `sudo apt install nvm`
@@ -13,3 +18,6 @@ Linux:
 `nvim` - may have errors, that's ok
 `:PlugInstall`
 exit nvim and re-open, should see lsp support downloaded
+
+
+
--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -86,3 +86,19 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+-- Disable tab jumps after switching out of insert
+function leave_snippet()
+    if
+        ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+        require('luasnip').unlink_current()
+    end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua leave_snippet()
+]])
